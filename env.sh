5878840bf30479b1e69bca911684e4792e54c93a
Seddon Shen <283481855@qq.com>
2025-02-20 19:46:25 +0800

    feat(XFUZZ): set up environment and integrate cover points
    - Create env.sh script to set up environment variables
    - Update FuzzTop.scala to include CoverPoint transforms
    - Move ccover subproject to new commit

---
+++ env.sh
@@ -0,0 +1,7 @@
+# XFUZZ Environment Variables
+export NOOP_HOME=$(pwd)
+export XFUZZ_HOME=$(pwd)/ccover
+
+# OSS CAD Suite
+export OSS_CAD_SUITE_HOME=$(pwd)/../oss-cad-suite/environment
+export COVER_POINTS_OUT=$(pwd)
--- generator/chisel3/src/main/scala/FuzzTop.scala
+++ generator/chisel3/src/main/scala/FuzzTop.scala
@@ -3,6 +3,7 @@ package freechips.rocketchip.system
 import chisel3.stage.{ChiselCli, ChiselGeneratorAnnotation, ChiselStage}
 import firrtl.options.Shell
 import firrtl.stage.FirrtlCli
+import xfuzz.CoverPoint
 
 class FuzzStage extends ChiselStage {
   override val shell: Shell = new Shell("rocket-chip")
@@ -16,6 +17,6 @@ object FuzzMain {
       ChiselGeneratorAnnotation(() => {
         freechips.rocketchip.diplomacy.DisableMonitors(p => new SimTop()(p))(new FuzzConfig)
       })
-    ))
+    ) ++ CoverPoint.getTransforms(args)._2)
   }
 }
