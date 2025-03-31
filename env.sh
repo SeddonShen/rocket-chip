e791e3b75abe2a010a1155f674e77067645c4801
LChooaa <LChooaa@outlook.com>
2025-03-31 15:11:52 +0800

    feat: update environment variables for corpus paths

--- env.sh
+++ env.sh
@@ -22,7 +22,8 @@ export CSR_TRANSITION_DIR=$(pwd)/ccover/SetInitValues
 # export CSR_SNAPSHOT=$(pwd)/ccover/SetInitValues/csr_snapshot
 
 # riscv corpus
-export RISCV_CORPUS=$(pwd)/corpus/footprints/riscv-tests
+export LINEARIZED_CORPUS=$(pwd)/corpus/linearized/riscv-tests
+export FOOTPRINTS_CORPUS=$(pwd)/corpus/footprints/riscv-tests
 # export RISCV_CORPUS=$(pwd)/corpus/linearized/riscv-tests
 
 # RTL Source and Destination
