dccf543b8cf8a95cdf80fe2cf3465f1d614d9a13
LChooaa <LChooaa@outlook.com>
2025-04-23 15:25:13 +0800

    feat: delete extra bytes in rom

--- src/main/scala/devices/tilelink/BootROM.scala
+++ src/main/scala/devices/tilelink/BootROM.scala
@@ -22,7 +22,9 @@ case class BootROMParams(
   address: BigInt = 0x10000,
   size: Int = 0x10000,
   hang: BigInt = 0x10040, // The hang parameter is used as the power-on reset vector
-  contentFileName: String)
+  contentFileName: String,
+  withDTB: Boolean = true,
+  )
 
 class TLROM(val base: BigInt, val size: Int, contentsDelayed: => Seq[Byte], executable: Boolean = true, beatBytes: Int = 4,
   resources: Seq[Resource] = new SimpleDevice("rom", Seq("sifive,rom0")).reg("mem"))(implicit p: Parameters) extends LazyModule
@@ -79,7 +81,8 @@ object BootROM {
     lazy val contents = {
       val romdata = Files.readAllBytes(Paths.get(params.contentFileName))
       val rom = ByteBuffer.wrap(romdata)
-      rom.array() ++ subsystem.dtb.contents
+      val dtb_contents = if (params.withDTB) subsystem.dtb.contents else Seq()
+      rom.array() ++ dtb_contents
     }
 
     val bootrom = bootROMDomainWrapper {
